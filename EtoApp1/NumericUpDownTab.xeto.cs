0b38d1ae869a401b72e205d56d20a1a5c753ed43
Jon <jon.se5a@gmail.com>
2016-10-08 13:55:46 +1300

    update to 2.3 stable

---
+++ EtoApp1/NumericUpDownTab.xeto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EtoApp1
+{
+    public class NumericUpDownTab : Panel
+    {
+        public NumericUpDownTab()
+        {
+            XamlReader.Load(this);
+        }
+    }
+
+    public class NumUpDnVM : INotifyPropertyChanged
+    {
+        public double MaxValue { get; set; } = 100;
+        public double MinValue { get; set; } = 0;
+        private double _value = 50;
+        public double Value { get { return _value; } set { _value = value; OnPropertyChanged(); } }
+        public double Increment { get; set; } = 1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
